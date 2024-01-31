0e10e9c08198de3c32eacd98684d85a6e8f0d94e
EmilKhamatnurov <107917011+EmilKhamatnurov@users.noreply.github.com>
2024-01-31 23:15:57 +0500

    Добавлен checkbox к услугам, рефакторинг

--- Personal-training-site/src/components/ Services/ Services.jsx
+++ Personal-training-site/src/components/ Services/ Services.jsx
@@ -10,12 +10,15 @@ function Services({ onClick, checkedService }) {
 				// checkedService === service.title ? { ...styles.service, ...styles.service_checked } : styles.service 
 				<div className={checkedService == service.title ? styles.service_checked : styles.service} key={service.id}>
 					<div className={styles.service__header}>
-						<p className={styles.service__title}>{service.title}</p>
-						<p className={styles.service__price}>{service.price}</p>
+						<div className="service__name">
+							<p className={styles.service__title}>{service.title}</p>
+							<p className={styles.service__price}>{service.price}</p>
+						</div>
+						<input className={styles.service__checkbox} type="radio" name="checked" checked={checkedService == service.title} readOnly={true} />
 					</div>
 					<p className={styles.service__description}>{service.description}</p>
 					{/* Список параметров услуги */}
-					<ul className={styles.service__list}>
+					{/* <ul className={styles.service__list}>
 						<li className={styles.service__item}>
 							<img src="/check-icon.svg" /> {" "}
 							{service.list[0]}
@@ -28,7 +31,7 @@ function Services({ onClick, checkedService }) {
 							<img src="/check-icon.svg" /> {" "}
 							{service.list[2]}
 						</li>
-					</ul>
+					</ul> */}
 					{/* Кнопка выбрать */}
 					<button onClick={() => onClick(service.title)} className={styles.service__button}>Выбрать</button>
 					{/* {service.label && (<img className={styles.service__label} src="/popular-label.png" />)} */}
